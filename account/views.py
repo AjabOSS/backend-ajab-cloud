1e1af8196d09cbea8aca34061fdd358a314c68b4
tiktaak <aramina1414@gmail.com>
2023-10-16 13:27:57 -0700

    add token to register api response

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 from myuser.models import MyUser
 from .serializers import *
@@ -13,6 +14,11 @@ class UserRegisterAPI(APIView):
 
         ser_data = UserRegisterSerializer(data=request.POST)
         if ser_data.is_valid():
-            ser_data.create(ser_data.validated_data)
-            return Response(ser_data.data, status=status.HTTP_201_CREATED)
+            user = ser_data.create(ser_data.validated_data)
+            token = Token.objects.create(user=user)
+            sd = dict(ser_data.data)
+            sd['token'] = str(token)
+            return Response(sd, status=status.HTTP_201_CREATED)
         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
