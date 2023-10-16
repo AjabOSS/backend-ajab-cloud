9c4ad2f03ea961dbf1d48b6cf42e673d933882fd
tiktaak <aramina1414@gmail.com>
2023-10-16 02:38:42 -0700

    add api-token-auth for login

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
 from .views import *
 
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path("register/", UserRegisterAPI.as_view())
+    path("register/", UserRegisterAPI.as_view()),
+    path("api-token-auth/", obtain_auth_token),
 ]
