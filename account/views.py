d73403d47dff91cddc831e3e6ebd3a9188718990
tiktaak <aramina1414@gmail.com>
2023-10-16 14:48:01 -0700

    register api AllowAny

--- account/views.py
+++ account/views.py
@@ -4,14 +4,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
 
 from myuser.models import MyUser
 from .serializers import *
 
 
 class UserRegisterAPI(APIView):
+    permission_classes = [AllowAny,]
     def post(self, request):
-
         ser_data = UserRegisterSerializer(data=request.POST)
         if ser_data.is_valid():
             user = ser_data.create(ser_data.validated_data)
@@ -20,5 +21,3 @@ class UserRegisterAPI(APIView):
             sd['token'] = str(token)
             return Response(sd, status=status.HTTP_201_CREATED)
         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    
