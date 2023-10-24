97c8fb5b248d674de1ef18cd3de0b2f16ada1a38
tiktaak <aramina1414@gmail.com>
2023-10-24 00:39:53 -0700

    Prevent email spamming in the development environment

--- account/utils.py
+++ account/utils.py
@@ -6,22 +6,21 @@ import uuid
 
 def send_verification_email(user):
         
-    evt_obj_to_delete = EmailConfirmationToken.objects.filter(user=user).delete()
+    EmailConfirmationToken.objects.filter(user=user).delete()
     evt_obj = EmailConfirmationToken.objects.create(user=user)    
     evt_obj.code = str(uuid.uuid4().hex[:6].upper())
 
     evt_obj.save()
     if settings.DEBUG:
         print(f"-- email confirmation code for {user.email} : {evt_obj.code}")
-        
-    data = {'t_code': str(evt_obj.code)}
-    message = get_template('confimation_email.txt').render(data)
-        
-    send_mail(
-        "کد تایید عجب کلود",
-        message,
-        settings.EMAIL_HOST_USER,
-        [user.email,],
-        fail_silently=False,
-        )
+    else:
+        data = {'t_code': str(evt_obj.code)}
+        message = get_template('confimation_email.txt').render(data)
+        send_mail(
+            subject="کد تایید عجب کلود",
+            message=message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user.email,],
+            fail_silently=False,
+            )
         
